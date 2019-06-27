09e4449fca1544c59041f36f439c5bdebf347fbd
mastruzzodavide@gmail.com <Davidu_007>
2019-06-27 11:42:44 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TextfieldComponent} from './components/textfield/textfield.component';
 
 const routes: Routes = [
-  {path: 'test', component: TextfieldComponent}
 ];
 
 @NgModule({
