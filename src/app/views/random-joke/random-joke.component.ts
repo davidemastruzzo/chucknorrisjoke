068beb7bbcb49d4f4086b8aef9b1b943da435d31
jan_preisig <jan_preisig@gmx.net>
2019-06-27 15:14:13 +0200

    connect random-joke with service and make everything work

--- src/app/views/random-joke/random-joke.component.ts
+++ src/app/views/random-joke/random-joke.component.ts
@@ -1,20 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Joke} from '../../models/joke';
+import {Subscription} from 'rxjs';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'app-random-joke',
   templateUrl: './random-joke.component.html',
   styleUrls: ['./random-joke.component.scss']
 })
-export class RandomJokeComponent implements OnInit {
-  currentJoke: Joke;
+export class RandomJokeComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  currentJoke: Joke = null;
 
-  constructor() {
+  constructor(private apiService: ApiService) {
   }
 
   ngOnInit() {
+    this.subscriptions.push(this.apiService.getRandomJoke().subscribe(joke => {
+      this.currentJoke = joke;
+    }));
   }
 
   newRandomJoke() {
+    this.subscriptions.push(this.apiService.getRandomJoke().subscribe(joke => {
+      this.currentJoke = joke;
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
   }
 }
