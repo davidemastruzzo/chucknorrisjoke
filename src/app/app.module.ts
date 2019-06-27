953ed50f8386a29580eeac96828b728d50ab4a06
jan_preisig <jan_preisig@gmx.net>
2019-06-27 13:24:59 +0200

    generate component model and add logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import {TextfieldComponent} from './components/textfield/textfield.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RandomJokeComponent } from './views/random-joke/random-joke.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextfieldComponent
+    TextfieldComponent,
+    RandomJokeComponent
   ],
   imports: [
     BrowserModule,
